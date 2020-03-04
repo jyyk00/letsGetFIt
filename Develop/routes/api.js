0aceb75c149fd95eee92786e400988a0d1ced35e
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-03 16:48:37 -0800

    api.js delete

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -45,3 +45,16 @@ router.post("/api/workouts", (req, res) => {
         res.json(err);
       });
   });
+
+  router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+      .then(() => {
+        res.json(true);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  module.exports = router;
+  
