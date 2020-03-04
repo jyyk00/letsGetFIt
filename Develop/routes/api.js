dbf83198543711376603873294b84e3b5742086d
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-03 16:48:07 -0800

    api.js get

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -25,3 +25,23 @@ router.post("/api/workouts", (req, res) => {
       });
   });
   
+  router.get("/api/workouts", (req, res) => {
+    Workout.find()
+      .then(dbWorkouts => {
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+      .then(dbWorkouts => {
+        console.log(dbWorkouts)
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
